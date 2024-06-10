b858faa9198f05d3234aa7b73872d07c519e02fd
sachi <sachi.gala@gmail.com>
2024-06-10 14:48:32 -0400

    Constructor

--- src/main/java/com/example/neueda_group27/model/CreditCard.java
+++ src/main/java/com/example/neueda_group27/model/CreditCard.java
@@ -8,5 +8,71 @@ public class CreditCard {
     private double creditLimit;
     private double balance;
     private int zipcode;
+
+    public CreditCard(String cardNumber, String cardHolderName, String expiryDate, int cvv, double creditLimit, double balance, int zipcode) {
+        this.cardNumber = cardNumber;
+        this.cardHolderName = cardHolderName;
+        this.expiryDate = expiryDate;
+        this.cvv = cvv;
+        this.creditLimit = creditLimit;
+        this.balance = balance;
+        this.zipcode = zipcode;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCardHolderName() {
+        return cardHolderName;
+    }
+
+    public void setCardHolderName(String cardHolderName) {
+        this.cardHolderName = cardHolderName;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(String expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public int getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(int cvv) {
+        this.cvv = cvv;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public void setCreditLimit(double creditLimit) {
+        this.creditLimit = creditLimit;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public int getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+    }
 }
 
