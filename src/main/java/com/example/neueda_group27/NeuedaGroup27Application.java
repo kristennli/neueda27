d396f0b6a88a6c4db9d5878c7f743acb4ec72170
Kristen Li <40070826+kristennli@users.noreply.github.com>
2024-06-10 10:55:04 -0700

    added Spring files

---
+++ src/main/java/com/example/neueda_group27/NeuedaGroup27Application.java
@@ -0,0 +1,13 @@
+package com.example.neueda_group27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuedaGroup27Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuedaGroup27Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/neueda_group27/NeuedaGroup27ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neueda_group27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuedaGroup27ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
