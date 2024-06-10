8db1331f5ede030282084ac898d3906792f925ef
Kristen Li <kristen.yc.li@gmail.com>
2024-06-10 14:43:01 -0400

    fields added

--- src/main/java/com/example/neueda_group27/model/CreditCard.java
+++ src/main/java/com/example/neueda_group27/model/CreditCard.java
@@ -1,12 +1,12 @@
 package com.example.neueda_group27.model;
 
 public class CreditCard {
-    private int id;
     private String cardNumber;
     private String cardHolderName;
     private String expiryDate;
     private int cvv;
     private double creditLimit;
     private double balance;
+    private int zipcode;
 }
 
