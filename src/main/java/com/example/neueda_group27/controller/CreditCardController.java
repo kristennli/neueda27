bcd267784a7db4cee24d74c252cf857a140535a5
Kristen Li <kristen.yc.li@gmail.com>
2024-06-11 14:05:01 -0400

    fixed 8080 server whitelabel error

--- src/main/java/com/example/neueda_group27/controller/CreditCardController.java
+++ src/main/java/com/example/neueda_group27/controller/CreditCardController.java
@@ -8,18 +8,35 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.neueda_group27.model.CreditCard;
 import com.example.neueda_group27.repository.CreditCardRepository;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/creditcard")
+@RequestMapping("/api/creditcards")
 public class CreditCardController {
     @Autowired
     private CreditCardRepository creditCardRepository;
+    
+    @Operation(summary = "Get all credit cards")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Found the credit cards"),
+        @ApiResponse(responseCode = "404", description = "Credit cards not found")
+    })
 
     @GetMapping
     public List<CreditCard> getAllCreditCards() {
         return creditCardRepository.findAll();
     }
+
+    @Operation(summary = "Get a credit card by its card number")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Found the credit card"),
+        @ApiResponse(responseCode = "404", description = "Credit card not found")
+    })
 
     @GetMapping("/{cardNumber}")
     public CreditCard getCreditCardByCardNumber(@PathVariable String cardNumber) {
